81a20d10533fcee69758a927bd599155eaa4121d
jetale <vishwajeet.bhosale21@alumni.colostate.edu>
2024-06-15 16:53:43 -0600

    changed name of main dir to align with proj name:

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_project',
+    name='postings_parser',
     version='0.1',
     packages=find_packages(),
     package_data={
-        '': ['data/*.csv'],  # Include all CSV files under the 'data' directory
+        '': ['postings_parser/input/*.txt'],  # Include all CSV files under the 'data' directory
     },
 )
