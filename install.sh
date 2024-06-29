fd2428f41543c603b881e208f9137ebc50e6ae55
jetale <vishwajeet.bhosale21@alumni.colostate.edu>
2024-06-28 22:58:54 -0600

    fixed typo in install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ VAR_NAME="PROJ_POSTINGS_PARSER_PATH"
 
 export VAR_NAME="$SCRIPT_DIR"
 echo "=> $VAR_NAME is set to $SCRIPT_DIR"
-echo "export PROJ_POSTINGS_PARSER=\"$SCRIPT_DIR\"" >> ~/.profile
+echo "export PROJ_POSTINGS_PARSER_PATH=\"$SCRIPT_DIR\"" >> ~/.profile
 source ~/.bashrc
 
 echo "=> Permanent export added to ~/.bashrc"
