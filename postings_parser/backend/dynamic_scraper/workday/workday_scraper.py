9af9687d0119a6b3f453d1c42a07331d26635609
jetale <vishwajeet.bhosale21@alumni.colostate.edu>
2024-07-21 14:39:10 -0600

    fixing typo in workday scraper related to wait.until

--- postings_parser/backend/dynamic_scraper/workday/workday_scraper.py
+++ postings_parser/backend/dynamic_scraper/workday/workday_scraper.py
@@ -28,13 +28,10 @@ class WorkdayScraper(BaseScraper):
         page: int = 1
         
         while (page < self.pages_to_scrape):
-            
             try:
                 job_elements = self.wait.until(
-                    EC.presence_of_element_located(
-                        (By.XPATH, '//li[@class="css-1q2dra3"]')
-                    )
-                )
+                            EC.presence_of_all_elements_located((By.XPATH, '//li[@class="css-1q2dra3"]'))
+                        )
             except Exception as e:
                 print(f"An error occurred while getting job elements for {company_name} on page->{page}: {str(e)}")
                 break
