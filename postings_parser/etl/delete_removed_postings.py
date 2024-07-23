0d88dea052bf098a6cdcd8b795696b59ab38e1e3
jetale <vishwajeet.bhosale21@alumni.colostate.edu>
2024-07-22 20:51:29 -0600

    rolling removed postings deleter

--- postings_parser/etl/delete_removed_postings.py
+++ postings_parser/etl/delete_removed_postings.py
@@ -2,7 +2,9 @@
 # visit each url one by one
 # see if the job posting exits
 # if not mark the row to be deleted
-import time
+import logging 
+
+from tqdm import tqdm
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -16,6 +18,7 @@ from postings_parser.utils.database_connector import Connector, ExecutionType
 class DeleteRemoved:
 
     def __init__(self):
+        self.logger = logging.getLogger("logger")
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
@@ -29,12 +32,22 @@ class DeleteRemoved:
         self.conn = Connector()
 
 
-    def check_if_exists(self, response):
+    def _delete_marked(self, to_be_deleted: list) -> None:
+        delete_query = "DELETE FROM postings WHERE job_id = ANY(%s);"
+        self.conn.execute_insert_query(
+            insert_query = delete_query, 
+            data = to_be_deleted,
+            type_execute = ExecutionType.MANY,
+            new_conn = True
+            )
+        self.logger.info(f"Deleted {len(to_be_deleted)} rows successfully")
+
+
+    def check_if_posting_exists(self, response) -> None:
         to_be_deleted: list = list()
-        for index, item in enumerate(response):
+        for item in tqdm(response, "Checking if postings still exist"):
             job_id = item[0]
             url = item[1]
-            print(url)
             self.driver.get(url)
             try:
                 self.wait.until(
@@ -49,20 +62,16 @@ class DeleteRemoved:
             try:
                 data_automation_id = element.get_attribute('data-automation-id')
                 if data_automation_id == "errorContainer":
-                    to_be_deleted.append(item)
-                print(data_automation_id)
+                    to_be_deleted.append(job_id)
             except:
                 continue
 
-            if index > 100:
-                exit()
-            time.sleep(0.5)
-
-        print(to_be_deleted)
+        if to_be_deleted:
+            self.logger.info(f"Deleting {len(to_be_deleted)} rows from postings table" )
+            self._delete_marked(to_be_deleted=to_be_deleted)
 
 
-
-    def get_all_urls(self):
+    def _get_all_urls(self) -> None:
         select_query = """
                     SELECT job_id, posting_url 
                     FROM postings
@@ -70,10 +79,11 @@ class DeleteRemoved:
 
         response = self.conn.execute_select_query(query=select_query)
         if response:
-            self.check_if_exists(response)
+            self.check_if_posting_exists(response)
 
 
-
+    def run(self) -> None:
+        self._get_all_urls()
 
 if __name__ == "__main__":
-    DeleteRemoved().get_all_urls()
+    DeleteRemoved().run()
