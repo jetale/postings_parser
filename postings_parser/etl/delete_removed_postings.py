6db19fc54a6491a70701288cdcdae14c99ea883f
jetale <vishwajeet.bhosale21@alumni.colostate.edu>
2024-07-22 19:05:40 -0600

    adding ETL module

---
+++ postings_parser/etl/delete_removed_postings.py
@@ -0,0 +1,64 @@
+# get all urls
+# visit each url one by one
+# see if the job posting exits
+# if not mark the row to be deleted
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+from postings_parser.utils.database_connector import Connector, ExecutionType
+
+
+class DeleteRemoved:
+
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--remote-debugging-pipe")
+        chrome_options.experimental_options["prefs"] = {
+            "profile.managed_default_content_settings.images": 2
+        }
+        self.driver = webdriver.Chrome(options=chrome_options)
+        self.wait = WebDriverWait(driver=self.driver, timeout=20)  # Adjust the timeout as needed
+        self.conn = Connector()
+
+
+    def check_if_exists(self, response):
+        to_be_deleted: list = list()
+        for item in response:
+            job_id = item[0]
+            url = item[1]
+            self.driver.get(url)
+            self.wait.until(
+                    EC.presence_of_element_located(
+                        (By.XPATH, '//li[@class="css-1vosg8"]')
+                    )
+                )
+            element = self.driver.find_element(By.XPATH, '//*[@id="mainContent"]/div/div')
+            try:
+                data_automation_id = element.get_attribute('data-automation-id')
+                print(data_automation_id)
+            except:
+                continue
+
+
+    def get_all_urls(self):
+        select_query = """
+                    SELECT job_id, posting_url 
+                    FROM postings
+                    LIMIT 10
+                    """
+
+        response = self.conn.execute_select_query(query=select_query)
+        if response:
+            self.check_if_exists(response)
+
+
+
+
+if __name__ == "__main__":
+    DeleteRemoved().get_all_urls()
---
+++ postings_parser/etl/s3_to_db.py
@@ -0,0 +1,183 @@
+import json
+import sys
+import logging
+from datetime import datetime
+from dataclasses import dataclass, fields, field
+from typing import Tuple, List, Optional
+
+import boto3
+from tqdm import tqdm
+from botocore.config import Config
+from bs4 import BeautifulSoup as bs
+
+from postings_parser.utils.general import generate_unique_id
+from postings_parser.utils.database_connector import Connector, ExecutionType
+
+
+@dataclass
+class PostingData:
+    job_id: str = field(default=str())
+    job_title: str = field(default=str())
+    company: str = field(default=str())
+    work_location: str = field(default=str())
+    posting_url: str = field(default=str())
+    posting_date: str = field(default=str())
+    parsed_date: str = field(default=str())
+    parsed_time: str = field(default='12:00:00')
+    job_description: str = field(default=str())
+    country: str = field(default=str())
+    is_remote: str = field(default=str())
+
+    def to_tuple(self) -> Tuple[str, ...]:
+        return tuple(getattr(self, field.name) for field in fields(self))
+
+    def get_column_names(self) -> Tuple[str, ...]:
+        return tuple(field.name for field in fields(self))
+    
+
+class MainDataLoader:
+    def __init__(self) -> None:
+        self.logger: logging.Logger = logging.getLogger("logger")
+        
+        self.bucket_name: str = "selenium-job-postings"
+        self.base_path: str = "postings/"
+        self.date_dir: str 
+        self.parsed_date: str = str()
+        self.posting_data_list: list = list()
+
+        client_config = Config(region_name="us-east-2")
+        self.s3_connector = boto3.client('s3', config=client_config)
+        self.db_connector = Connector()
+
+
+    def _insert_into_db(self):
+        insert_query = """
+                    INSERT INTO postings (
+                        job_id, job_title, company, work_location, posting_url, 
+                        posting_date, parsed_date, parsed_time, job_description, country, is_remote
+                    )
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    ON CONFLICT (job_id) DO UPDATE SET
+                        posting_date = EXCLUDED.posting_date,
+                        job_description = EXCLUDED.job_description,
+                        country = EXCLUDED.country,
+                        is_remote = EXCLUDED.is_remote;
+                """
+        self.db_connector.execute_insert_query(
+            insert_query=insert_query,
+            data=self.posting_data_list, 
+            type_execute=ExecutionType.MANY,
+            new_conn=True)
+
+    def _extract_script_elements(
+            self, 
+            url,
+            company_name,
+            job_title_tag,
+            job_description_tag,
+            script_element
+            ):
+        
+        posting = PostingData()
+        json_content = script_element.string
+        data: dict = json.loads(json_content)
+
+        posting.job_title = job_title_tag.get('content')
+        posting.company = company_name
+        posting.job_description = job_description_tag.get('content')
+        posting.country = data.get('jobLocation', {}).get('address', {}).get('addressCountry', 'All')
+        posting.work_location = data.get('jobLocation', {}).get('address', {}).get('addressLocality', 'All')
+        posting.is_remote = data.get('jobLocationType', None)
+        posting.posting_date = data.get('datePosted', None)
+        posting.parsed_date = self.parsed_date
+        #employment_type = data.get('employmentType', None)
+        #req_id = data.get('identifier', {}).get('value', None)
+        posting.job_id = generate_unique_id(
+            job_title=posting.job_title, 
+            company_name=company_name,
+            job_href=url)
+        
+        self.posting_data_list.append(posting.to_tuple())
+
+
+    def _extract_posting_info(self, company_name: str, url: str, posting_html: str):
+        soup = bs(posting_html, 'html.parser')
+        job_title_tag = soup.find('meta', {'name': 'title', 'property': 'og:title'})
+        job_description_tag = soup.find('meta', {'name':'description', 'property':'og:description'})
+        script_tag = soup.find('script', type='application/ld+json')
+
+        if script_tag and job_title_tag and job_description_tag:
+            self._extract_script_elements(
+                url=url,
+                company_name=company_name,
+                job_title_tag=job_title_tag,
+                job_description_tag=job_description_tag,
+                script_element=script_tag
+                ) 
+           
+
+    def _parse_object(self, file_obj: dict) -> None:
+        file_content = file_obj['Body'].read()
+        file_content_dict = json.loads(file_content.decode('utf-8'))
+        company_name =  list(file_content_dict.keys())[0]
+        for key, val in file_content_dict[company_name].items():
+            self._extract_posting_info(
+                company_name=company_name, 
+                url=key, 
+                posting_html=val)
+  
+
+    def _get_objects(self, objects: list) -> None:
+        for obj in tqdm(objects, "Parsing Objects"):
+            file_obj = self.s3_connector.get_object(Bucket=self.bucket_name, Key=obj)
+            self._parse_object(file_obj) 
+           
+        self._insert_into_db()
+
+    def _get_objects_list(self, date_dir: str) -> None:
+        prefix: str = self.base_path + date_dir + "/" 
+        paginator = self.s3_connector.get_paginator('list_objects_v2')
+        page_iterator = paginator.paginate(Bucket=self.bucket_name, Prefix=prefix)
+        
+        objects: list = []
+        for page in page_iterator:
+            if 'Contents' in page:
+                for obj in page['Contents']:
+                    objects.append(obj['Key'])
+        
+        if objects:
+            self._get_objects(objects)
+        else:
+            self.logger.error(f"Latest date directory does not contain any objects. {date_dir}  Exiting ---")
+
+
+    def _list_date_directories(self) -> list:
+        paginator = self.s3_connector.get_paginator('list_objects_v2')
+        page_iterator = paginator.paginate(Bucket=self.bucket_name, Prefix=self.base_path, Delimiter='/')
+        
+        date_directories: list = []
+        for page in page_iterator:
+            if 'CommonPrefixes' in page:
+                for cp in page['CommonPrefixes']:
+                    date_directories.append(cp['Prefix'].split('/')[-2])
+        
+        return date_directories
+    
+    def _get_latest_date_directory(self)-> None:
+        date_directories: list = self._list_date_directories()
+        date_directories.sort(reverse=True, key=lambda date: datetime.strptime(date, '%Y-%m-%d'))
+
+        if date_directories:
+            self.parsed_date = date_directories[0]
+            self._get_objects_list(date_directories[0])
+        else:
+            self.logger.error("Could not get any date directories-- Exiting")
+
+    
+    def main(self):
+        self._get_latest_date_directory()
+
+if __name__ == "__main__":
+    #scalene_profiler.start()
+    MainDataLoader().main()
+    #scalene_profiler.stop()
